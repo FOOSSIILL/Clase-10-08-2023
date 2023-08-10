7fa957cd0643d0bf1f6ea21d6bc563e2f66d214c
SofíaMG <98538133+FOOSSIILL@users.noreply.github.com>
2023-08-10 13:33:51 -0500

    node_modules

---
+++ math.js
@@ -0,0 +1,18 @@
+/*function add(a,b) {
+    return a + b;
+}*/
+
+const add2 = (a,b) =>{
+    return a + b;
+}
+
+const substract = (a,b) => {
+    return a = b;
+}
+
+//const add3 = (a,b) => a + b;
+
+module.exports = {
+    add,
+    substract
+}
---
+++ wow.js
@@ -0,0 +1,8 @@
+const math = require('./math.js');
+
+console.log(math.substract);
+const result = math.substract(1,2);
+console.log(result);
+
+const lodash = require ('lodash');
+console.log(lodash.capitalize('pedro'));
